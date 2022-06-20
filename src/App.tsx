30be3fca5acc16730c210c509ff449c580a3dde8
leandrocodes <leandrovianacodes@gmail.com>
2022-06-20 18:12:52 -0400

    :package: using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import { useState } from 'react'
-import { Grocery } from './components/Grocery'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import { Login } from './pages/Login'
 
 function App() {
-	const [names, setNames] = useState<string[]>(['lettuce', 'onion', 'tomato'])
-
-	function createContent() {
-		setNames([...names, 'carrot'])
-	}
-
 	return (
-		<>
-			{names.map(content => {
-				return <Grocery name={content} />
-			})}
-			<button onClick={createContent}> Add Grocery </button>
-		</>
+		<Router>
+			<Routes>
+				<Route path='/' element={<Login />} />
+			</Routes>
+		</Router>
 	)
 }
 
