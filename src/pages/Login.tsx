2f62476dc44f6625bb79e7402d93bc144da2a9d0
leandrocodes <leandrovianacodes@gmail.com>
2022-06-20 18:38:48 -0400

    :package: configuring eslint

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,15 @@
+import { Input, Stack } from '@chakra-ui/react'
+
 export function Login() {
-	return <h1>Login</h1>
+	return (
+		<Stack spacing={6}>
+			<label htmlFor='Login Email'></label>
+			<Input
+				name='Login Email'
+				variant='flushed'
+				size={'sm'}
+				placeholder='email@example.com'
+			/>
+		</Stack>
+	)
 }
