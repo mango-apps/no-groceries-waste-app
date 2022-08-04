af8ba7462cd17091eaf6829fe666cd57e33b0ff3
leandrocodes <leandrovianacodes@gmail.com>
2022-08-04 15:23:58 -0400

    new login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { ChakraProvider } from '@chakra-ui/react'
+
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import green from '@mui/material/colors/green'
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: green[400]
+		}
+	}
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<ChakraProvider>
+		<ThemeProvider theme={theme}>
 			<App />
-		</ChakraProvider>
+		</ThemeProvider>
 	</React.StrictMode>
 )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,106 +1,137 @@
-import { useState } from 'react'
-import {
-	Flex,
-	Heading,
-	Input,
-	Button,
-	InputGroup,
-	Stack,
-	InputLeftElement,
-	chakra,
-	Box,
-	Link,
-	Avatar,
-	FormControl,
-	FormHelperText,
-	InputRightElement
-} from '@chakra-ui/react'
-import { FaUserAlt, FaLock } from 'react-icons/fa'
+import * as React from 'react'
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import TextField from '@mui/material/TextField'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Checkbox from '@mui/material/Checkbox'
+import Link from '@mui/material/Link'
+import Paper from '@mui/material/Paper'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import KitchenIcon from '@mui/icons-material/Kitchen'
+import Typography from '@mui/material/Typography'
 
-const CFaUserAlt = chakra(FaUserAlt)
-const CFaLock = chakra(FaLock)
+function Copyright(props: any) {
+	return (
+		<Typography
+			variant='body2'
+			color='text.secondary'
+			align='center'
+			{...props}
+		>
+			{'Copyright © '}
+			<Link color='inherit' href='https://mui.com/'>
+				Your Website
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	)
+}
 
 export function Login() {
-	const [showPassword, setShowPassword] = useState(false)
-
-	const handleShowClick = () => setShowPassword(!showPassword)
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
+		const data = new FormData(event.currentTarget)
+		console.log({
+			email: data.get('email'),
+			password: data.get('password')
+		})
+	}
 
 	return (
-		<Flex
-			flexDirection='column'
-			width='100wh'
-			height='100vh'
-			bgGradient='linear(to-b, orange.200, red.300)'
-			justifyContent='center'
-			alignItems='center'
-		>
-			<Stack
-				flexDir='column'
-				mb='2'
-				justifyContent='center'
-				alignItems='center'
-			>
-				<Avatar bg='blue.700' />
-				<Heading color='blue.700'>No waste your Groceries!</Heading>
-				<Box minW={{ base: '90%', md: '468px' }}>
-					<form>
-						<Stack
-							spacing={4}
-							paddingX={'2.15rem'}
-							paddingY={'2.25rem'}
-							backgroundColor='whiteAlpha.900'
-							boxShadow='md'
-							borderRadius={'md'}
+		<Grid container component='main' sx={{ height: '100vh' }}>
+			<CssBaseline />
+			<Grid
+				item
+				xs={false}
+				sm={4}
+				md={7}
+				sx={{
+					backgroundImage:
+						'url(https://images.unsplash.com/photo-1579113800032-c38bd7635818?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3456&q=80)',
+					backgroundRepeat: 'no-repeat',
+					backgroundColor: t =>
+						t.palette.mode === 'light'
+							? t.palette.grey[50]
+							: t.palette.grey[900],
+					backgroundSize: 'cover',
+					backgroundPosition: 'center'
+				}}
+			/>
+			<Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+				<Box
+					sx={{
+						my: 8,
+						mx: 4,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center'
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+						<KitchenIcon />
+					</Avatar>
+					<Typography component='h1' variant='h5'>
+						Sign in
+					</Typography>
+					<Typography component={'p'}>for manage your fridge</Typography>
+					<Box
+						component='form'
+						noValidate
+						onSubmit={handleSubmit}
+						sx={{ mt: 1 }}
+					>
+						<TextField
+							margin='normal'
+							required
+							fullWidth
+							id='email'
+							label='Email Address'
+							name='email'
+							autoComplete='email'
+							autoFocus
+						/>
+						<TextField
+							margin='normal'
+							required
+							fullWidth
+							name='password'
+							label='Password'
+							type='password'
+							id='password'
+							autoComplete='current-password'
+						/>
+						<FormControlLabel
+							control={<Checkbox value='remember' color='primary' />}
+							label='Remember me'
+						/>
+						<Button
+							type='submit'
+							fullWidth
+							variant='contained'
+							sx={{ mt: 3, mb: 2 }}
 						>
-							<FormControl>
-								<InputGroup>
-									<InputLeftElement
-										pointerEvents='none'
-										children={<CFaUserAlt color='gray.300' />}
-									/>
-									<Input type='email' placeholder='email address' />
-								</InputGroup>
-							</FormControl>
-							<FormControl>
-								<InputGroup>
-									<InputLeftElement
-										pointerEvents='none'
-										color='gray.300'
-										children={<CFaLock color='gray.300' />}
-									/>
-									<Input
-										type={showPassword ? 'text' : 'password'}
-										placeholder='Password'
-									/>
-									<InputRightElement width='4.5rem'>
-										<Button h='1.75rem' size='sm' onClick={handleShowClick}>
-											{showPassword ? 'Hide' : 'Show'}
-										</Button>
-									</InputRightElement>
-								</InputGroup>
-								<FormHelperText textAlign='right'>
-									<Link>forgot password?</Link>
-								</FormHelperText>
-							</FormControl>
-							<Button
-								borderRadius={'md'}
-								type='submit'
-								variant='solid'
-								colorScheme='blue'
-								width='full'
-							>
-								Login
-							</Button>
-						</Stack>
-					</form>
+							Sign In
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link href='#' variant='body2'>
+									Forgot password?
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link href='#' variant='body2'>
+									{"Don't have an account? Sign Up"}
+								</Link>
+							</Grid>
+						</Grid>
+						<Copyright sx={{ mt: 5 }} />
+					</Box>
 				</Box>
-			</Stack>
-			<Box color={'whiteAlpha.900'}>
-				New to us?{' '}
-				<Link color='blue.700' href='#'>
-					Sign Up
-				</Link>
-			</Box>
-		</Flex>
+			</Grid>
+		</Grid>
 	)
 }
